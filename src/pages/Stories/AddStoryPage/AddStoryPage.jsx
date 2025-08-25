0ab840cf021fca543161ecf9ad2cab8d2ca8ec53
husainsaleh123 <hussainsaleh180@gmail.com>
2025-08-26 01:26:28 +0300

    fixed error in AddStoryPage.jsx

--- src/pages/Stories/AddStoryPage/AddStoryPage.jsx
+++ src/pages/Stories/AddStoryPage/AddStoryPage.jsx
@@ -1,7 +1,7 @@
 // src/pages/Stories/AddStoryPage.jsx
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import StoryForm from "../../components/StoryForm";
+import StoryForm from "../../components/Stories/StoryForm";
 import { createStory } from "../../utilities/stories-api";
 import styles from "./AddStoryPage.module.scss";
 
